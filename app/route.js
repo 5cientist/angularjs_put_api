4c35ff40fec9dd8b161006713cf8524c93b4bc6f
hanshir <hanshir@coder.domain.name>
2020-07-06 22:41:31 +0530

    modified

---
+++ app/route.js
@@ -0,0 +1,19 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "main.html"
+    })
+    .when("/name", {
+        templateUrl : "api/putData.php"
+    })
+   
+});
+function getResultsPage(){
+    $http({
+        url: URL + 'api/getData.php',
+        method: 'GET'
+     }).then(function(res){
+        $scope.data = res.data.data;
+     });
+}
